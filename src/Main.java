e4f258590a80d17f91b5b9aafad16844a04312f9
raisknk <gustavosialer@iclou.com>
2020-10-15 15:54:10 -0700

    my first comit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public  static void main(String[] args) {
+
+        int[] array = new int[] { 1, 2, 3, 4, 5 };
+        int x = array[4];
+        System.out.println(x);
+
+
+
+
+    }
+}
